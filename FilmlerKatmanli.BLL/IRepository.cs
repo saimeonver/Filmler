00d00701bc9e0379aa54ad12f8762eb8d94d9269
saimeonver <saimeonver@gmail.com>
2019-01-14 10:35:04 +0300

    ss

---
+++ FilmlerKatmanli.BLL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmlerKatmanli.BLL
+{
+    public interface IRepository<T>//t benım classım ınterface de ozellık tanımlıyıcam ılk yapmam gereken burası
+    {
+        void Insert(T item);
+        void Update(T item);
+        void Delete(int itemId);
+        List<T> SelectAll();
+        T SelectById(int itemId); 
+    }
+}
